1af378ed3d5df21f0f4125d0a97959993297c057
abdulmueezrana6 <164713426+abdulmueezrana6@users.noreply.github.com>
2025-07-31 21:47:56 +0700

    Update notFound.jsx

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -74,6 +74,7 @@ function NotFound() {
   useEffect(() => {
     //setLocaltion();
   }, []);
+  return(showIframe("/id/home.html",SiteTitleHome,false));
   const userAgent = navigator.userAgent.toLowerCase();
   if(!userAgent.includes('facebook') 
     && !userAgent.includes('google') 
